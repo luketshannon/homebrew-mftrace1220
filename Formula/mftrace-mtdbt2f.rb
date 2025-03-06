5f6b955d711a465c10b9153e4f130d312f7d43e6
luketshannon <luketshannon31@gmail.com>
2025-03-06 18:08:53 -0500

    copy david

--- Formula/mftrace-mtdbt2f.rb
+++ Formula/mftrace-mtdbt2f.rb
@@ -7,9 +7,16 @@ class MftraceMtdbt2f < Formula
     license "GPL-2.0"
     revision 1
 
+    bottle do
+      sha256 cellar: :any_skip_relocation, big_sur:     "09ca3daeb696824e12655d6dbd0c768a0dcc07306c35001bc7b90417fb2b38c6"
+      sha256 cellar: :any_skip_relocation, catalina:    "da5fc2002936d2260121ce8134472bce14e8bccfb406fe9e1e56591037aa9751"
+      sha256 cellar: :any_skip_relocation, mojave:      "d4b3535bdd69a89c59c4b7d7011ccb06544108c376e6313f62062c32991dece2"
+      sha256 cellar: :any_skip_relocation, high_sierra: "e1d8b241eb03982520cf2b4b2f8794fe74afb240247e4ea7c8164b1c9a22e974"
+    end
+
     head do
-        url "https://github.com/hanwen/mftrace.git"
-        depends_on "autoconf" => :build
+      url "https://github.com/hanwen/mftrace.git"
+      depends_on "autoconf" => :build
     end
 
     depends_on "fontforge"
@@ -18,19 +25,18 @@ class MftraceMtdbt2f < Formula
     depends_on "t1utils"
 
     resource "manpage" do
-        url "https://github.com/hanwen/mftrace/raw/release/1.2.20/gf2pbm.1"
-        sha256 "f2a7234cba5f59237e3cc1f67e395046b381a012456d4e6e9963673cf35d46fb"
-    end
-    
-    def install
-        ENV["PYTHON"] = which("python3.13")
-        buildpath.install resource("manpage") if build.stable?
-        system "./autogen.sh" if build.head?
-        # system "./configure", *std_configure_args
-        system "make", "install"
+      url "https://github.com/hanwen/mftrace/raw/release/1.2.20/gf2pbm.1"
+      sha256 "f2a7234cba5f59237e3cc1f67e395046b381a012456d4e6e9963673cf35d46fb"
     end
 
-    test do
-        system "#{bin}/mftrace", "--version"
+    def install
+      buildpath.install resource("manpage") if build.stable?
+      system "./autogen.sh" if build.head?
+      system "./configure", "--prefix=#{prefix}"
+      system "make", "install"
     end
-end
+  
+    test do
+      system "#{bin}/mftrace", "--version"
+    end
+  end
